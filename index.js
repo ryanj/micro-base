3c4552f6893b85406951ab509e4e4e4cd4a70608
ryan jarvinen <ryan.jarvinen@gmail.com>
2018-01-09 21:35:30 -0800

    now serving: index.html & static files (#1)

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const {send} = require('micro')
+const {send} = require('micro'),
+          fs = require('fs-extra')
 
 module.exports = async (req, res) => {
-  const statusCode = 200
-  const data = { message: "Hello from 'The Future'!" }
-
-  send(res, statusCode, data)
+  //strip querystring
+  const url = req.url.split('?')[0];
+  if(url.search('/static/') == 0 || url.search('/node_modules/') == 0){
+    try{
+      const staticFile = await fs.readFile(__dirname + url)
+      //sniffing mime type from 'accept' header?
+      const mimeType = req.headers.accept.split(',')[0];
+      if(mimeType != "*/*"){
+        res.setHeader('Content-Type', mimeType)
+      }
+      send(res, 200, staticFile)
+    } catch(err) {
+      res.setHeader('Content-Type', "text/html")
+      send(res, 404, `404: ${req.url} NotFound`)
+    }
+  }else if(url == "/" || url == "/index.html"){
+    const indexHtml = await fs.readFile(__dirname + '/index.html')
+    res.setHeader('Content-Type', "text/html")
+    send(res, 200, indexHtml);
+  }else{
+    res.setHeader('Content-Type', "text/html")
+    send(res, 404, `404: ${req.url} NotFound`)
+  }
 }
