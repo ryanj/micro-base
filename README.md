6a449977f4711a3568f2e364e5728a45a5c1b20a
ryan jarvinen <ryan.jarvinen@gmail.com>
2018-01-10 14:07:55 -0800

    adding OpenShift dev notes (#3)

--- README.md
+++ README.md
@@ -3,18 +3,32 @@
 
 [![npm dependency statuses](http://img.shields.io/david/ryanj/micro-base.svg "npm dependencies, via david-dm")](https://david-dm.org/ryanj/micro-base)
 
-## Local Development
-For a local development environment, clone the repo and run:
+## Local Dev
+Clone the repo for a local development environment, then install dependencies and launch with:
 
 ```bash
+npm install
 npm run dev
 ```
 
-## Production
-NodeJS V8 or higher is required:
+## Developing with OpenShift
+
+Use NodeJS v8.5.0 or higher when running this code:
 
 ```bash
-npm start
+oc new-app -e NPM_RUN=dev ryanj/nodejs-demo:8~https://github.com/ryanj/micro-base
+```
+
+Expose a route to your app to review changes while you work:
+
+```bash
+oc expose svc/micro-base && xdg-open http://$(oc get route micro-base | grep -v NAME | awk '{print $2}')
+```
+
+Connect to a pod using a live terminal in the OpenShift dashboard, or continuously sync code from a local clone of this repo into your remotely-running container:
+
+```bash
+oc rsync -w --exclude='.git*,node_modules*' $(pwd)/ $(oc get pods | grep -v NAME | grep -v build | cut -f1 -d' '):/opt/app-root/src/
 ```
 
 ## License: MIT
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const {send} = require('micro'),
-          fs = require('fs-extra')
+         fse = require('fs-extra');
+
+// DIY readFile example
+//const fs = require('fs')
+//const readFile = (path, opts = 'utf8') =>
+//    new Promise((res, rej) => {
+//        fs.readFile(path, opts, (err, data) => {
+//            if (err) rej(err)
+//            else res(data)
+//        })
+//    })
 
 module.exports = async (req, res) => {
   //strip querystring
   const url = req.url.split('?')[0];
   if(url.search('/static/') == 0 || url.search('/node_modules/') == 0){
     try{
-      const staticFile = await fs.readFile(__dirname + url)
+      const staticFile = await fse.readFile(__dirname + url)
       //sniffing mime type from 'accept' header?
       const mimeType = req.headers.accept.split(',')[0];
       if(mimeType != "*/*"){
@@ -18,7 +28,7 @@ module.exports = async (req, res) => {
       send(res, 404, `404: ${req.url} NotFound`)
     }
   }else if(url == "/" || url == "/index.html"){
-    const indexHtml = await fs.readFile(__dirname + '/index.html')
+    const indexHtml = await fse.readFile(__dirname + '/index.html')
     res.setHeader('Content-Type', "text/html")
     send(res, 200, indexHtml);
   }else{
